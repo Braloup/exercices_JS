87969af10acf78224936ce1a9138174ca9e85f52
VieuxLoupGris <provocemois@gmail.com>
2019-01-21 12:22:40 +0100

    Create a Story Teller. Three questions asked for start a story

---
+++ exercice_1.8.js
@@ -0,0 +1,15 @@
+const readLineSync = require("readline-sync");
+
+let numberOfQuestion = 3;
+
+let userFirstNamneChoice = readLineSync.question("Enter the first name you wish to start your pretty story: ");
+numberOfQuestion -= 1;
+console.log(numberOfQuestion  + " more questions");
+
+let userActionChoice = readLineSync.question("Enter a conjugated verb to start the action of your pretty story: ");
+numberOfQuestion -= 1;
+console.log(numberOfQuestion + " more question");
+
+let userComplementChoice = readLineSync.question("Enter a complement to bring your pretty story to life: ");
+
+console.log(userFirstNamneChoice + " " + userActionChoice + " " + userComplementChoice + ".");
