e1ebfa9705feff3b174d85c9e46e50c5d69b17b6
VieuxLoupGris <provocemois@gmail.com>
2019-01-21 12:31:57 +0100

    Ask to the user to write an exciting sentence

--- exercice_1.8.js
+++ exercice_1.8.js
@@ -1,15 +1,17 @@
 const readLineSync = require("readline-sync");
 
-let numberOfQuestion = 3;
+let numberOfQuestion = 4;
 
 let userFirstNamneChoice = readLineSync.question("Enter the first name you wish to start your pretty story: ");
-numberOfQuestion -= 1;
-console.log(numberOfQuestion  + " more questions");
+console.log((numberOfQuestion -= 1) + " more questions");
 
 let userActionChoice = readLineSync.question("Enter a conjugated verb to start the action of your pretty story: ");
-
 console.log((numberOfQuestion -= 1) + " more question");
 
 let userComplementChoice = readLineSync.question("Enter a complement to bring your pretty story to life: ");
+console.log((numberOfQuestion -= 1) + " more question");
+
+let addAString = readLineSync.question("Enter a phase so that your pretty story becomes exciting: ");
 
 console.log(userFirstNamneChoice + " " + userActionChoice + " " + userComplementChoice + ".");
+console.log(addAString);
