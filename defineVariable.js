44f5908746c394d12777d090d740eba754dfac01
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:36:38 +0100

    Add documentation

--- defineVariable.js
+++ defineVariable.js
@@ -1,2 +1,8 @@
+
+
+/*
+**Define a variable and display it in the console.
+*/
+
 let maVariable = 11;
 console.log(maVariable);
--- exercice_1.8.js
+++ exercice_1.8.js
@@ -11,7 +11,7 @@ console.log((numberOfQuestion -= 1) + " more question");
 let userComplementChoice = readLineSync.question("Enter a complement to bring your pretty story to life: ");
 console.log((numberOfQuestion -= 1) + " more question");
 
-let addAString = readLineSync.question("Enter a phase so that your pretty story becomes exciting: ");
+let addAString = readLineSync.question("Enter a sentence so that your pretty story becomes exciting: ");
 
 console.log(userFirstNamneChoice + " " + userActionChoice + " " + userComplementChoice + ".");
 console.log(addAString);
