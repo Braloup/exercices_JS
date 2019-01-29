fee9d4be5467ddf23b8f9292d0ad4e15889f7c88
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:41:32 +0100

    Add documentation

--- exercice_1.3.js
+++ exercice_1.3.js
@@ -2,7 +2,7 @@
 **Ask the user to enter its first name and display a message saying "Hello first name".
 */
 
-const readLineSync = require("readline-sync");
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let userFirstName = readLineSync.question("Enter your first name please. ");
 
--- exercice_1.4.js
+++ exercice_1.4.js
@@ -1,7 +1,13 @@
-const readLineSync = require("readline-sync");
+/*
+**Make this same thing as Exercise 2 but this time ask the user for the name, the first name and the city.
+*/
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let userName = readLineSync.question("What is your name? ");
+
 let userFirstName = readLineSync.question("What is your firstname? ");
+
 let userCity = readLineSync.question("Where do you come from? ")
 
 console.log("You are " + userFirstName + " " + userName + ". And you're come from " + userCity + ".");
