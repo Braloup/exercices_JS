8935d9aed4b0a3fec98ebf8f2f9315967184eed4
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:45:14 +0100

    Add documentation

--- exercice_1.6.js
+++ exercice_1.6.js
@@ -1,4 +1,8 @@
-const readLineSync = require("readline-sync");
+/*
+**Ask two integers to the user. Display the rest of the integer division of the two numbers.
+*/
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let firstNumber = readLineSync.question("Please enter a float nunber: ");
 let secondNumber = readLineSync.question("Please enter a second float number: ");
