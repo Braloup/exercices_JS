26f4212566c83fef5f1cc6878ce19fce170d4c03
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:43:39 +0100

    Add documentation

--- exercice_1.5.js
+++ exercice_1.5.js
@@ -1,6 +1,13 @@
-const readLineSync = require("readline-sync");
+/*
+**Ask two numbers with decimal part to the user. For the first one only keep the integer part. Then multiply them and display the result.
+
+**You will have to perform a search on Google to know how to only keep the integer part of a number.
+*/
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let firstNumber = readLineSync.question("Please enter a float nunber: ");
+
 let secondNumber = readLineSync.question("Please enter a second float number: ");
 
 let totalNumber = parseInt(firstNumber) * secondNumber;
