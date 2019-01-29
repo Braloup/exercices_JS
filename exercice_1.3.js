ec938d047d051fdf01a1ae25ed48e814363abe8d
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:39:18 +0100

    Add documentation

--- exercice_1.3.js
+++ exercice_1.3.js
@@ -1,3 +1,7 @@
+/*
+**Ask the user to enter its first name and display a message saying "Hello first name".
+*/
+
 const readLineSync = require("readline-sync");
 
 let userFirstName = readLineSync.question("Enter your first name please. ");
