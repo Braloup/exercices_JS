772976cb4d96a0d82fe8799d2aefa1c7fdd65969
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:49:50 +0100

    Add documentation

--- exercice_1.8.js
+++ exercice_1.8.js
@@ -1,4 +1,13 @@
-const readLineSync = require("readline-sync");
+/*
+*Create a "Story Teller".
+
+**Ask questions to the user about different things. Store the results he gives to you. Display them in a way that it creates a fun story!
+
+**Bonus: when you ask questions to the user he should be able to see how many questions remain to be asked.
+*/
+
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let numberOfQuestion = 4;
 
@@ -12,6 +21,6 @@ let userComplementChoice = readLineSync.question("Enter a complement to bring yo
 console.log((numberOfQuestion -= 1) + " more question");
 
 let addAString = readLineSync.question("Enter a sentence so that your pretty story becomes exciting: ");
-
 console.log(userFirstNamneChoice + " " + userActionChoice + " " + userComplementChoice + ".");
+
 console.log(addAString);
