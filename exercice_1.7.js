46234cfbd6e0685ed52e9d3915f8415e1e32d1ba
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:47:03 +0100

    Add documentation

--- exercice_1.7.js
+++ exercice_1.7.js
@@ -1,6 +1,19 @@
-const readLineSync = require("readline-sync");
+/*
+**Ask to the user its shoe size and its birth year. Then make the following calculation:
+
+**Multiply the shoe size by 2
+**Add 5 to the result
+**Multiply that by 50
+**Subtract the birth year
+**Add 1766
+
+**Test with your own birth year and your shoe size.
+*/
+
+const readLineSync = require("readline-sync");//Call the module to ask question to the user.
 
 let userShoesSize = readLineSync.question("Please enter your shoes size: ");
+
 let userBierthYear = readLineSync.question("Please enter your birth year: ");
 
 let finalNumber = ((((userShoesSize * 2) + 5) * 50) - userBierthYear) + 1766;
