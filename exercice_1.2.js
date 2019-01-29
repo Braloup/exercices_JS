ecb8fe338a04b94a5d8c994cdb2b8384c2568543
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 14:38:12 +0100

    Add documentation

--- exercice_1.2.js
+++ exercice_1.2.js
@@ -1,3 +1,7 @@
+/*
+**Define three variables: name, firstName and city. Display them like this: "Your name is Gerard Lambert and you live in Paris.".
+*/
+
 let name = "Goldstein";
 let firstName = "Emmanuel ";
 let city = "Liège";
